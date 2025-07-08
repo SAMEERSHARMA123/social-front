74f43ead7ad50df633995a82eb2430e971956a83
SAMEERSHARMA123 <jangidsameer20@gmail.com>
2025-07-08 11:29:41 +0530

    Update RegisterForm.js

--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -97,7 +97,7 @@ const RegisterForm = () => {
         phone: formData.phone,
       };
       const response = await axios.post(
-        'social-back-sigma.vercel.app/graphql',
+        'https://social-back-sigma.vercel.app/graphql',
         { query, variables },
         { headers: { 'Content-Type': 'application/json' }, withCredentials: true }
       );
